537e2a0b54e8f3556923d2dcacc9a77ad202cf4f
Danila Fedorin <nothingnut@gmail.com>
2014-04-27 18:17:28 -0700

    Added close method to save log upon exit

--- src/com/danife/ircserver/userinterface/GUI.java
+++ src/com/danife/ircserver/userinterface/GUI.java
@@ -1,12 +1,47 @@
 package com.danife.ircserver.userinterface;
 
+import java.awt.BorderLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.io.FileNotFoundException;
+import java.io.PrintWriter;
+import java.io.UnsupportedEncodingException;
+import java.util.Calendar;
+
+import javax.swing.JFrame;
+import javax.swing.JTextArea;
+import javax.swing.JTextField;
+
+import com.danife.ircserver.server.Startup;
 
 public class GUI implements ActionListener{
+	private JFrame frame = new JFrame("DanilaFe's IRC Server");
+	private JTextArea area = new JTextArea();
+	private JTextField field = new JTextField();
+	private PrintWriter pwriter;
+	private Startup s;
 	
-	public GUI(){
+	public GUI(Startup startup){
+		s = startup;
 		
+		try {
+			pwriter = new PrintWriter("log-" + Calendar.getInstance().getTime().toString(), "UTF-8");
+		} catch (FileNotFoundException | UnsupportedEncodingException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		
+		frame.setSize(500,500);
+		frame.setDefaultCloseOperation(frame.EXIT_ON_CLOSE);
+		frame.setLayout(new BorderLayout());
+		
+		frame.add(area, BorderLayout.CENTER);
+		frame.add(field, BorderLayout.SOUTH);
+		
+		area.setEditable(false);
+		
+		frame.setVisible(true);
 	}
 	
 	@Override
@@ -14,5 +49,21 @@ public class GUI implements ActionListener{
 	
 		
 	}
+	
+	/**
+	 * Adds a new line to the log. It is automatically time-stamped.
+	 * @param line the line to add
+	 */
+	public void addLogLine(String line){
+		Calendar c = Calendar.getInstance();
+		area.append("[" + c.get(Calendar.HOUR) + ":" + c.get(Calendar.MINUTE) + ":" + c.get(Calendar.SECOND)+ "] " + line + "\n");
+		pwriter.println("[" + c.get(Calendar.HOUR) + ":" + c.get(Calendar.MINUTE) + ":" + c.get(Calendar.SECOND)+ "] " + line);
+	}
 
+	/**
+	 * Shuts down the gui.
+	 */
+	public void close(){
+		pwriter.close();
+	}
 }

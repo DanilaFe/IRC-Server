b04c178b938474fd92980a92b615a863c34d08d1
Danila Fedorin <nothingnut@gmail.com>
2014-04-30 20:35:33 -0700

    Added simple class to get the IP.

---
+++ src/com/danilafe/ircserver/util/IP.java
@@ -0,0 +1,32 @@
+package com.danilafe.ircserver.util;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.URL;
+
+public class IP {
+	
+	private String myip;
+	
+	public IP() {
+		try {
+			URL ipget = new URL("http://icanhazip.com");
+			BufferedReader in = new BufferedReader(new InputStreamReader(ipget.openStream()));
+			myip = in.readLine();
+			System.out.println(myip);
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+	
+	/**
+	 * Returns the received ip.
+	 * @return
+	 */
+	public String getIP(){
+		return myip;
+	}
+
+}
